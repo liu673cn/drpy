8b55047d38a83654e10c10223b87d4de808ff3d3
hjdhnx <hjd124579>
2022-08-25 16:26:50 +0800

    新增555影视

---
+++ js/555影视.js
@@ -0,0 +1,11 @@
+var rule = {
+    title:'555影视',
+    url:'https://www.5dy5.cc/vodshow/fyclass--------fypage---.html',
+    searchUrl:'/vodsearch/**----------fypage---.html',
+    ua:'MOBILE_UA',
+    class_name:'电影&连续剧&福利&动漫&综艺',
+    class_url:'1&2&124&4&3',
+    一级:'body a.module-poster-item.module-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+    二级:{"title":"h1&&Text;.module-info-tag&&Text","img":".lazyload&&data-original","desc":".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text","content":".module-info-introduction&&Text","tabs":".module-tab-item","lists":".module-play-list:eq(#id) a"},
+    搜索:'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',
+}
