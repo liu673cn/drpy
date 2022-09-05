aa8d053868c6596ae93c3b229d30b8ea7d814d47
hjdhnx <hjd124579>
2022-09-06 02:22:53 +0800

    增加了升级日志打印

--- utils/update.py
+++ utils/update.py
@@ -111,5 +111,5 @@ def download_new_version():
         msg = '升级成功'
     except Exception as e:
         msg = f'升级失败:{e}'
-    logger.info(f'参数检验js读取共计耗时:{get_interval(t1)}毫秒')
+    logger.info(f'系统升级共计耗时:{get_interval(t1)}毫秒')
     return msg
