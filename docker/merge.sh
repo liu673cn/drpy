df9bc009d3dcd3f0b49ff25388c843fa864ee31c
hjdhnx <hjd124579>
2022-09-07 09:28:16 +0800

    增加合并脚本

---
+++ docker/merge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#kill -9 $(cat supervisord.pid) # 杀掉进程
+docker manifest create hjdhnx/drpy hjdhnx/drpy:amd64-v3.2.1 hjdhnx/drpy:arm64-v3.2.1 hjdhnx/drpy:armv7-v3.2.1
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:amd64-v3.2.1 --os linux --arch amd64
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:arm64-v3.2.1 --os linux --arch arm64/v8
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:armv7-v3.2.1 --os linux --arch arm/v7
+docker manifest push hjdhnx/drpy
+# 保留一个 bash
+#/bin/bash
