f4517ba5e349aecca0d444c61c6e29ec16f7db60
hjdhnx <hjd124579>
2022-09-10 16:21:35 +0800

    发布新的镜像

--- docker/merge.sh
+++ docker/merge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #kill -9 $(cat supervisord.pid) # 杀掉进程
-docker manifest create hjdhnx/drpy hjdhnx/drpy:amd64-v3.4.1 hjdhnx/drpy:arm64-v3.4.1 hjdhnx/drpy:armv7-v3.4.1
-docker manifest annotate hjdhnx/drpy hjdhnx/drpy:amd64-v3.4.1 --os linux --arch amd64
-docker manifest annotate hjdhnx/drpy hjdhnx/drpy:arm64-v3.4.1 --os linux --arch arm64/v8
-docker manifest annotate hjdhnx/drpy hjdhnx/drpy:armv7-v3.4.1 --os linux --arch arm/v7
+docker manifest create hjdhnx/drpy hjdhnx/drpy:amd64-v3.4.8 hjdhnx/drpy:arm64-v3.4.8 hjdhnx/drpy:armv7-v3.4.8
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:amd64-v3.4.8 --os linux --arch amd64
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:arm64-v3.4.8 --os linux --arch arm64/v8
+docker manifest annotate hjdhnx/drpy hjdhnx/drpy:armv7-v3.4.8 --os linux --arch arm/v7
 docker manifest push hjdhnx/drpy
 # 保留一个 bash
 #/bin/bash
