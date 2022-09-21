3d29c8b3c7f7d48d8ec5a05ffbaac6d9060faa94
hjdhnx <hjd124579>
2022-09-21 19:26:52 +0800

    修复鸭奈飞源

--- js/鸭奈飞.js
+++ js/鸭奈飞.js
@@ -1,6 +1,6 @@
 var rule = Object.assign(muban.mxpro,{
 title:'鸭奈飞',
 host:'https://yanetflix.com',
-url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+url:'/vodshow/fyclass--------fypage---.html',
 class_parse:'.navbar-items li:gt(1):lt(6);a&&Text;a&&href;.*/(.*?).html',
 });
