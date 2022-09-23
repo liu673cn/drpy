8b14848826ea61816c69948e1943d16d7fa79518
hjdhnx <hjd124579>
2022-09-23 21:35:54 +0800

    优酷搜索完成

--- libs/pre.js
+++ libs/pre.js
@@ -221,7 +221,6 @@ function maoss(jxurl, ref, key) {
 
 function request(url,obj){
     // obj = obj||{'user-agent': MOBILE_UA};
-    print('224');
     let new_obj;
     if(typeof(fetch_params)!=='undefined'){
         new_obj = obj?Object.assign(fetch_params,obj):fetch_params;
@@ -242,7 +241,6 @@ function request(url,obj){
         }
         return html
     }
-    print('245');
     return ''
 }
 
