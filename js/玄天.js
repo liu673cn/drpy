b8bc3083ba5ccb5dcce312e7a8d91927ad834660
hjdhnx <hjd124579>
2022-09-23 15:15:52 +0800

    正常处理了玄天源的数据

--- js/玄天.js
+++ js/玄天.js
@@ -1,4 +1,5 @@
 muban.首图2.二级.tabs = '.dropdown-menu li';
+muban.首图2.二级.重定向='js:let url = jsp.pd(html,"a.btn-primary&&href");log(url);html = request(url)';
 var rule = Object.assign(muban.首图2,{
     title:'玄天',
     host:'https://m.7caa.com',
